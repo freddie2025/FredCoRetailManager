92af3edea6dd6f5a24c8d69367afe2ec0f87c767
Fred James <fredjsam@live.co.uk>
2020-02-05 15:37:08 +0000

    Add the Web API Project
    Adds a blank Web API project and tweak it

---
+++ FRMDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ FRMDataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FRMDataManager.WebApiApplication" Language="C#" %>
