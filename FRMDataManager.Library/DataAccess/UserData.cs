ba6df2387264a7eb6d3b170ac23dd0000a768081
Fred James <fredjsam@live.co.uk>
2020-02-06 15:14:05 +0000

    Add User Data Library
    Created ORM classes using swagger. Called user table via SP using the API.

---
+++ FRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using FRMDataManager.Library.Internal.DataAccess;
+using FRMDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace FRMDataManager.Library.DataAccess
+{
+	public class UserData
+	{
+		public List<UserModel> GetUserById(string Id)
+		{
+			SqlDataAccess sql = new SqlDataAccess();
+
+			var p = new { Id = Id };
+
+			var output = sql.LoadData<UserModel, dynamic>("[dbo].[spUserLookup]", p, "FRMData");
+
+			return output;
+		}
+	}
+}
---
+++ FRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using FRMDataManager.Library.DataAccess;
+using FRMDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace FRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
