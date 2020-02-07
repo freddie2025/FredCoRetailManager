2fa255b22e55b3b16850720caa095c1c6a591a90
Fred James <fredjsam@live.co.uk>
2020-02-07 17:48:56 +0000

    Replace Container Reference In ShellVM
    Replace container reference with the IoC reference instead.

--- FRMDesktopUI/ViewModels/ShellViewModel.cs
+++ FRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -6,19 +6,16 @@ namespace FRMDesktopUI.ViewModels
 	public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
 	{
 		private SalesViewModel _salesVM;
-		private SimpleContainer _container;
 		private IEventAggregator _events;
 
-		public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, 
-			SimpleContainer container)
+		public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
 		{
 			_events = events;
 			_salesVM = salesVM;
-			_container = container;
 
 			_events.Subscribe(this);
 
-			ActivateItem(_container.GetInstance<LoginViewModel>());
+			ActivateItem(IoC.Get<LoginViewModel>());
 		}
 
 		public void Handle(LogOnEvent message)
